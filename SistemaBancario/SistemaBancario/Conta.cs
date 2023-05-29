d3fbd7ae7e75ae2f4cfa7f1eee5f66b1dcf61e6c
Emerson de Araújo Alexandre <65199861+emersondearaujo@users.noreply.github.com>
2023-05-29 00:31:51 -0300

    Add files via upload

---
+++ SistemaBancario/SistemaBancario/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario
+{
+    public class Conta
+    {
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public String NomeConta { get; set; }
+        public Titular Titular { get; set; }
+        public Decimal Saldo { get; set; }
+        public Decimal VerSaldo()
+        {
+            return Saldo;
+        }
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(decimal valor)
+        {
+            if (valor <= Saldo)
+                Saldo -= valor;
+            else
+                Console.WriteLine("Saque não pode ser realizado, saldo insuficiente");
+        }
+    }
+}
---
+++ SistemaBancario/SistemaBancario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var conta = new Conta();
+            conta.NomeConta = "Conta Teste";
+            conta.NumeroConta = 1000;
+            conta.Agencia = 001;
+            conta.Saldo = 1000;
+
+            conta.Titular = new Titular()
+            {
+                Cpf = "00000000000",
+                Rg = "00000000000",
+                Endereco = "Rua Central",
+                Nome = "Emerson de Araujo"
+            };
+            Console.WriteLine($"O seu saldo atual é: {conta.VerSaldo()}");
+
+            conta.Depositar(100);
+
+            Console.WriteLine($"O seu saldo atual, após o deposito é: {conta.VerSaldo()}");
+
+            conta.Sacar(500);
+
+            Console.WriteLine($"O seu saldo atual, após o saque é: {conta.VerSaldo()}");
+
+            conta.Sacar(3000);
+
+            Console.ReadKey();
+        }
+    }
+}
